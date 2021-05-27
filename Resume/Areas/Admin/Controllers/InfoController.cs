d947a92f0e7b83a37c6d30a21755f807f9ee3459
Orxan Məmmədəli <memmedeli.orxan.om@gmail.com>
2021-05-27 16:00:08 +0400

    27.05.002

--- Resume/Areas/Admin/Controllers/InfoController.cs
+++ Resume/Areas/Admin/Controllers/InfoController.cs
@@ -20,72 +20,13 @@ namespace Resume.Areas.Admin
             _context = context;
         }
 
-        // GET: Admin/Info
         public async Task<IActionResult> Index()
         {
-            return View(await _context.Infos.ToListAsync());
+            return View(await _context.Infos.FirstOrDefaultAsync());
         }
 
-        // GET: Admin/Info/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
 
-            var info = await _context.Infos
-                .FirstOrDefaultAsync(m => m.ID == id);
-            if (info == null)
-            {
-                return NotFound();
-            }
-
-            return View(info);
-        }
-
-        // GET: Admin/Info/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Admin/Info/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("ID,Name,Surname,Speciality,Email,Telephone,Adress,BirthDate,Profil,FotoURL,Coordinates")] Info info)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(info);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(info);
-        }
-
-        // GET: Admin/Info/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var info = await _context.Infos.FindAsync(id);
-            if (info == null)
-            {
-                return NotFound();
-            }
-            return View(info);
-        }
-
-        // POST: Admin/Info/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
+        [HttpPost, ActionName("Index")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("ID,Name,Surname,Speciality,Email,Telephone,Adress,BirthDate,Profil,FotoURL,Coordinates")] Info info)
         {
@@ -115,35 +56,6 @@ namespace Resume.Areas.Admin
                 return RedirectToAction(nameof(Index));
             }
             return View(info);
-        }
-
-        // GET: Admin/Info/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var info = await _context.Infos
-                .FirstOrDefaultAsync(m => m.ID == id);
-            if (info == null)
-            {
-                return NotFound();
-            }
-
-            return View(info);
-        }
-
-        // POST: Admin/Info/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var info = await _context.Infos.FindAsync(id);
-            _context.Infos.Remove(info);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
         }
 
         private bool InfoExists(int id)
