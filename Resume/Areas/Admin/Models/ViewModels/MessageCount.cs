a12f46c6af5d638b305d31bca75532d7a818b3d2
Orxan Məmmədəli <memmedeli.orxan.om@gmail.com>
2021-06-08 15:57:50 +0400

    08.06.001

---
+++ Resume/Areas/Admin/Models/ViewModels/MessageCount.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Resume.Areas.Admin.Models.ViewModels
+{
+    public class MessageCount
+    {
+        public int Inbox { get; set; }
+        public int Read { get; set; }
+        public int Answered { get; set; }
+    }
+}
---
+++ Resume/Areas/Admin/ViewComponents/MessageAsideViewComponent.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Resume.Areas.Admin.Models.ViewModels;
+using Resume.Models.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Resume.Areas.Admin.ViewComponents
+{
+    public class MessageAsideViewComponent : ViewComponent
+    {
+        private readonly MessageCount _count;
+        private readonly ResumeContext db;
+
+        public MessageAsideViewComponent(MessageCount count, ResumeContext context)
+        {
+            _count = count;
+            db = context;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var messages = db.Contacts;
+            _count.Inbox = messages.Where(s => s.Respons == false && s.Status == true).Count();
+            _count.Answered = messages.Where(s => s.Respons == true && s.Status == false).Count();
+            _count.Read = messages.Where(s => s.Respons == false && s.Status == false).Count();
+            return View(_count);
+        }
+    }
+}
