6291c4429489c095c78b3677b767934a637a2a47
Orxan Məmmədəli <memmedeli.orxan.om@gmail.com>
2021-06-15 22:07:31 +0400

    15.06.002

---
+++ Resume/Areas/Admin/Controllers/ActionNamesController.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Resume.Models.Context;
+using Resume.Models.Entities;
+
+namespace Resume.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ActionNamesController : Controller
+    {
+        private readonly ResumeContext _context;
+
+        public ActionNamesController(ResumeContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            ViewBag.Controller = id;
+            var resumeContext = _context.ActionNames.Where(x => x.ControllerNamesID == id).Include(a => a.ControllerNames);
+            return View(await resumeContext.ToListAsync());
+        }
+
+
+        public IActionResult Create(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            ViewBag.Controller = id;
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,Name,ControllerNamesID")] ActionNames actionNames)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(actionNames);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewBag.Controller = actionNames.ControllerNamesID;
+            return View(actionNames);
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var actionNames = await _context.ActionNames.FindAsync(id);
+            if (actionNames == null)
+            {
+                return NotFound();
+            }
+            ViewBag.Controller = actionNames.ControllerNamesID;
+            return View(actionNames);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Name,ControllerNamesID")] ActionNames actionNames)
+        {
+            if (id != actionNames.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(actionNames);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ActionNamesExists(actionNames.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewBag.Controller = actionNames.ControllerNamesID;
+            return View(actionNames);
+        }
+
+        // GET: Admin/ActionNames/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var actionNames = await _context.ActionNames
+                .Include(a => a.ControllerNames)
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (actionNames == null)
+            {
+                return NotFound();
+            }
+
+            return View(actionNames);
+        }
+
+        // POST: Admin/ActionNames/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var actionNames = await _context.ActionNames.FindAsync(id);
+            _context.ActionNames.Remove(actionNames);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ActionNamesExists(int id)
+        {
+            return _context.ActionNames.Any(e => e.ID == id);
+        }
+    }
+}
---
+++ Resume/Areas/Admin/Controllers/ControllerActionUsersController.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Resume.Models.Context;
+using Resume.Models.Entities;
+
+namespace Resume.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ControllerActionUsersController : Controller
+    {
+        private readonly ResumeContext _context;
+
+        public ControllerActionUsersController(ResumeContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var resumeContext = _context.ControllerActionUsers.Include(c => c.ControllerNames).Include(c => c.User);
+            return View(await resumeContext.ToListAsync());
+        }
+
+
+        public IActionResult Create()
+        {
+            ViewData["ControllerID"] = new SelectList(_context.ControllerNames, "ID", "ID");
+            ViewData["UserID"] = new SelectList(_context.Users, "ID", "ID");
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ControllerID,UserID")] ControllerActionUser controllerActionUser)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(controllerActionUser);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ControllerID"] = new SelectList(_context.ControllerNames, "ID", "ID", controllerActionUser.ControllerID);
+            ViewData["UserID"] = new SelectList(_context.Users, "ID", "ID", controllerActionUser.UserID);
+            return View(controllerActionUser);
+        }
+
+        // GET: Admin/ControllerActionUsers/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var controllerActionUser = await _context.ControllerActionUsers.FindAsync(id);
+            if (controllerActionUser == null)
+            {
+                return NotFound();
+            }
+            ViewData["ControllerID"] = new SelectList(_context.ControllerNames, "ID", "ID", controllerActionUser.ControllerID);
+            ViewData["UserID"] = new SelectList(_context.Users, "ID", "ID", controllerActionUser.UserID);
+            return View(controllerActionUser);
+        }
+
+        // POST: Admin/ControllerActionUsers/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ControllerID,UserID")] ControllerActionUser controllerActionUser)
+        {
+            if (id != controllerActionUser.ControllerID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(controllerActionUser);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ControllerActionUserExists(controllerActionUser.ControllerID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ControllerID"] = new SelectList(_context.ControllerNames, "ID", "ID", controllerActionUser.ControllerID);
+            ViewData["UserID"] = new SelectList(_context.Users, "ID", "ID", controllerActionUser.UserID);
+            return View(controllerActionUser);
+        }
+
+        // GET: Admin/ControllerActionUsers/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var controllerActionUser = await _context.ControllerActionUsers
+                .Include(c => c.ControllerNames)
+                .Include(c => c.User)
+                .FirstOrDefaultAsync(m => m.ControllerID == id);
+            if (controllerActionUser == null)
+            {
+                return NotFound();
+            }
+
+            return View(controllerActionUser);
+        }
+
+        // POST: Admin/ControllerActionUsers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var controllerActionUser = await _context.ControllerActionUsers.FindAsync(id);
+            _context.ControllerActionUsers.Remove(controllerActionUser);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ControllerActionUserExists(int id)
+        {
+            return _context.ControllerActionUsers.Any(e => e.ControllerID == id);
+        }
+    }
+}
