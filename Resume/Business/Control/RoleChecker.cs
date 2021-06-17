6c1d59614590ce789a718a8a1e3257c5eba2c184
Orxan Məmmədəli <memmedeli.orxan.om@gmail.com>
2021-06-17 14:13:30 +0400

    17.06.002

--- Resume/Business/Control/RoleChecker.cs
+++ Resume/Business/Control/RoleChecker.cs
@@ -12,11 +12,11 @@ namespace Resume.Business.Control
         public static bool AuthorizeRoles(ResumeContext context, int userID, string controllerName, string actionName)
         {
             bool result = false;
-            var role = context.ControllerActionUsers.FirstOrDefault(x => x.UserID == userID && x.ControllerNames.Name == controllerName);
-            if (role != null)
-            {
-                var control = context.ActionNames.FirstOrDefault(x => x.ControllerNamesID == role.ControllerID && x.Name == actionName);
-                if (control != null)
+            var controllers = context.ControllerActionUsers.FirstOrDefault(x => x.UserID == userID && x.ControllerNames.Name == controllerName);
+            var action = context.ActionNames.FirstOrDefault(x => x.ControllerNamesID == controllers.ControllerID && x.Name == actionName);
+            if (controllers != null && action != null)
+            {            
+                if (context.ActionUsers.FirstOrDefault(x => x.UserID == userID && x.ActionID == action.ID) != null)
                 {
                     result = true;
                 }
